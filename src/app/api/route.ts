36cafbed17b6aad82f27d6d226ffd47e79c8b2f6
AlecBlance <17378596+AlecBlance@users.noreply.github.com>
2024-03-15 23:28:59 +0800

    chore: 💀 add comments

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -3,21 +3,31 @@ import JSZip from "jszip";
 const newZip = new JSZip();
 
 export const POST = async (req: Request) => {
+  // * Regex for tags to be deleted
   const sheetRegex = /<sheetProtection(.*?)\/>/g;
   const workBookRegex = /<workbookProtection(.*?)\/>/g;
+
+  // * Getting the file from the form
   const formData = await req.formData();
   const file = formData.get("file") as unknown as File;
   const buffer = await file.arrayBuffer();
-  const zip = await JSZip.loadAsync(buffer);
 
+  // * Loading the xlsx file
+  const zip = await JSZip.loadAsync(buffer);
   for (const [filePath, zipObject] of Object.entries(zip.files)) {
+    // * Getting the contents of each file inside xlsx
     let content = await zipObject.async("string");
+    // * Removing tags from content
     content = content.replaceAll(sheetRegex, "").replaceAll(workBookRegex, "");
+    // * Adding the file to the zip
     newZip.file(filePath, content);
   }
 
-  const unlockedZip = await newZip.generateAsync({ type: "blob" });
-  return new Response(unlockedZip, {
+  // * Generate zip blob
+  const unlockedExcel = await newZip.generateAsync({ type: "blob" });
+
+  // * Returning the zip as spreedsheet
+  return new Response(unlockedExcel, {
     status: 200,
     headers: {
       "content-type":
